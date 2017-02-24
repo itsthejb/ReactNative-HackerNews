af5a978bb2860e8705c80397dd01ec4c4270135f
Jonathan Crooke <jonathan.crooke@zalando.de>
2017-02-24 13:08:34 +0100

    Switch react versions

---
+++ Home.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import React, { Component } from 'react';
+// import {
+//   // AppRegistry,
+//   // StyleSheet,
+//   // Text,
+//   View
+// } from 'react-native';
+
+class Home extends Component {
+  render() {
+    return null;
+    // return (
+    //   <View style={styles.container}>
+    //     <Text style={styles.welcome}>
+    //       Welcome to React Native!
+    //     </Text>
+    //     <Text style={styles.instructions}>
+    //       To get started, edit index.ios.js
+    //     </Text>
+    //     <Text style={styles.instructions}>
+    //       Press Cmd+R to reload,{'\n'}
+    //       Cmd+D or shake for dev menu
+    //     </Text>
+    //   </View>
+    // );
+  }
+}
+
+module.exports = Home;
