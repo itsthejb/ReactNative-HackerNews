d61ffe3e6eb5f41d632c6ea00397c1e06756ef63
Jonathan Crooke <jonathan.crooke@zalando.de>
2017-02-24 13:08:34 +0100

    Switch react versions

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,36 @@
  * @flow
  */
 
+'use strict';
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  NavigatorIOS,
   Text,
   View
 } from 'react-native';
 
-export default class HackerNews extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
+const home = require('./Home');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
+
+export default class HackerNews extends Component {
+  render() {
+    <NavigatorIOS
+      style={styles.container}
+      initailRoute={{
+      title: 'HackerNews',
+      component: home
+    }}/>
+  }
+}
 
 AppRegistry.registerComponent('HackerNews', () => HackerNews);
