2b093a56beea80b3c9bb2816b9cd1eea3f3b1692
Jonathan Crooke <jonathan.crooke@zalando.de>
2017-02-24 13:59:39 +0100

    MAking progress

--- Home.js
+++ Home.js
@@ -1,25 +1,36 @@
 'use strict';
 
 import React, { Component } from 'react';
-// import {
-//   // AppRegistry,
-//   // StyleSheet,
-//   // Text,
-//   View
-// } from 'react-native';
+import {
+  // AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+var ReactNative = require('react-native');
+var styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'blue',
+    fontSize: 30,
+    margin: 80
+  }
+});
 
 class Home extends Component {
   render() {
-    return null;
+    return <ReactNative.Text style={styles.text}>Hello World (Again)</ReactNative.Text>;
+
     // return (
-    //   <View style={styles.container}>
-    //     <Text style={styles.welcome}>
+    //   <View>
+    //     <Text style={styles.text}>
     //       Welcome to React Native!
     //     </Text>
-    //     <Text style={styles.instructions}>
+    //     <Text style={styles.text}>
     //       To get started, edit index.ios.js
     //     </Text>
-    //     <Text style={styles.instructions}>
+    //     <Text style={styles.text}>
     //       Press Cmd+R to reload,{'\n'}
     //       Cmd+D or shake for dev menu
     //     </Text>
