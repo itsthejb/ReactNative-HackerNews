3f76ae7000e5476f284dfcb07af8ede4627aac13
Jonathan Crooke <jonathan.crooke@zalando.de>
2017-02-24 14:51:35 +0100

    Group

--- Home.js
+++ Home.js
@@ -7,9 +7,10 @@ import {
   Text,
   View
 } from 'react-native';
+const ReactNative = require('react-native');
 
-var ReactNative = require('react-native');
-var styles = StyleSheet.create({
+const baseURL = "https://hacker-news.firebaseio.com/v0/";
+const styles = StyleSheet.create({
   text: {
     color: 'black',
     backgroundColor: 'blue',
@@ -20,23 +21,43 @@ var styles = StyleSheet.create({
 
 class Home extends Component {
   render() {
+    this._fetchTopStories();
     return <ReactNative.Text style={styles.text}>Hello World (Again)</ReactNative.Text>;
-
-    // return (
-    //   <View>
-    //     <Text style={styles.text}>
-    //       Welcome to React Native!
-    //     </Text>
-    //     <Text style={styles.text}>
-    //       To get started, edit index.ios.js
-    //     </Text>
-    //     <Text style={styles.text}>
-    //       Press Cmd+R to reload,{'\n'}
-    //       Cmd+D or shake for dev menu
-    //     </Text>
-    //   </View>
-    // );
   }
+
+  _fetchTopStories() {
+    fetch(baseURL + "topstories.json")
+      .then(response => response.json())
+      .then(json => this._fetchItems(json))
+      .catch(error => console.log(error));
+  };
+
+  _fetchItems(topStoryIDs) {
+    var results = new Map();
+    var promises = [];
+
+    for (var i = 0; i < 5; ++i) {
+      var id = topStoryIDs[i];
+      var url = baseURL + "item/" + id + ".json";
+
+      var promise = fetch(url)
+      .then(response => response.json())
+      .then(json => {
+        results.set(i, json);
+      })
+      .catch(error => console.log(error));
+
+      promises.push(promise);
+    }
+
+    Promise.all(promises).then(values => {
+      this._allItemsFetched(results);
+    });
+  };
+
+  _allItemsFetched(results) {
+    console.log(results);
+  };
 }
 
 module.exports = Home;
