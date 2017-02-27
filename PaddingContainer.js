b32a94021a756c84922b08aedd3044fcc075b1f5
Jonathan Crooke <jonathan.crooke@zalando.de>
2017-02-27 11:19:42 +0100

    Padding container

---
+++ PaddingContainer.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  View,
+  Navigator,
+  Text
+} from 'react-native';
+
+class PaddingContainer extends Component {
+  render () {
+    return (
+      <View style={
+        {
+          paddingTop: Navigator.NavigationBar.Styles.General.TotalNavHeight,
+          flex: 1
+        }
+      }>{this.props.child}</View>
+    )
+  }
+}
+
+module.exports = PaddingContainer;
