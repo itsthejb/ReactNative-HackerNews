1bf68f3ee4881f9915e3605aa1c26852fcc3da1b
Jonathan Crooke <jonathan.crooke@zalando.de>
2017-02-27 16:07:26 +0100

    Arch

--- Home/Home.js
+++ Home/Home.js
@@ -38,6 +38,7 @@ class Home extends Component {
   render() {
     var sectionControl = (
       <SectionControl
+        initialIndex={2}
         selectedIndex={this.state.selectedIndex}
         onChange={(index) => {this.setState({selectedIndex: index})}
       }/>
