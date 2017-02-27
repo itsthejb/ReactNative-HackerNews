ccac95458b416d36785603d0e2aec04b681b15e2
Jonathan Crooke <jonathan.crooke@zalando.de>
2017-02-27 15:30:00 +0100

    Section tabling

--- Home/SectionControl.js
+++ Home/SectionControl.js
@@ -1,19 +1,37 @@
 'use strict'
 
 import React, { Component } from 'react'
-import { SegmentedControlIOS } from ''
+import { SegmentedControlIOS } from 'react-native'
+const HackerNews = require('../Shared/HackerNews');
 
 class SectionControl extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedSegmentIndex: 0
+    };
+  }
+
   render() {
     return (
       <SegmentedControlIOS
-        values={['One', 'Two']}
-        selectedIndex={this.state.selectedIndex}
+        values={HackerNews.sections}
+        selectedIndex={this.state.selectedSegmentIndex}
         onChange={(event) => {
-          this.setState({selectedIndex: event.nativeEvent.selectedSegmentIndex});
+          var index = event.nativeEvent.selectedSegmentIndex
+          this.setState({selectedSegmentIndex: index});
+          this._callBackWithSectionIndex(index)
         }}
       />
     )
+  }
+
+  componentWillMount() {
+    this._callBackWithSectionIndex(this.state.selectedSegmentIndex)
+  }
+
+  _callBackWithSectionIndex(index) {
+    this.props.onChange(HackerNews.URLForSectionIndex(index))
   }
 }
 
--- Shared/HackerNews.js
+++ Shared/HackerNews.js
@@ -2,6 +2,14 @@ class HackerNews {
   static get baseURL() {
     return "https://hacker-news.firebaseio.com/v0/";
   }
+
+  static get sections() {
+    return ["New", "Top", "Best"]
+  }
+
+  static URLForSectionIndex(index) {
+    return this.baseURL
+  }
 }
 
 module.exports = HackerNews;
