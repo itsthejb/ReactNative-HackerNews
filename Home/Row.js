3bd9fb5b9800ed2dcdc1ea0ebc762fcf8fd026e6
Jonathan Crooke <jonathan.crooke@zalando.de>
2017-02-27 13:40:19 +0100

    Fetch articles

--- Home/Row.js
+++ Home/Row.js
@@ -3,32 +3,44 @@
 import React, { Component } from 'react';
 import {
   View,
+  ActivityIndicator,
   TouchableHighlight,
   Text
 } from 'react-native';
 
+const HackerNews = require('../Shared/HackerNews');
+
 class Row extends Component {
-  render() {
-    return (
-      <TouchableHighlight underlayColor='#f1c40f'>
-        <View>
-          <Text>{this.props.identifier}</Text>
-        </View>
-      </TouchableHighlight>
-    )
+  constructor(props) {
+    super(props)
+    this.state = { item: null }
   }
 
-  _fetchItem(id) {
-    fetch(baseURL + "item/" + id + ".json")
+  render() {
+    if (this.state.item) {
+      return (
+        <TouchableHighlight underlayColor='#f1c40f'>
+          <View>
+            <Text>{this.state.item.title}</Text>
+          </View>
+        </TouchableHighlight>
+      )
+    } else {
+      this._fetchItem()
+      return <ActivityIndicator size='small'/>;
+    }
+  }
+
+  _fetchItem() {
+    fetch(HackerNews.baseURL + "item/" + this.props.identifier + ".json")
     .then(response => response.json())
     .then(json => {
-      var ds = this.state.dataSource.cloneWithRows
-      results.push(json);
+      this.setState({
+        item: json
+      })
     })
     .catch(error => console.log(error));
   }
-
-
 }
 
 module.exports = Row;
