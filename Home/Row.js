be549bbefac5206173156365c1860d9cc700cdd0
Jonathan Crooke <jonathan.crooke@zalando.de>
2017-02-28 10:25:44 +0100

    Abort done

--- Home/Row.js
+++ Home/Row.js
@@ -33,14 +33,20 @@ class Row extends Component {
     }
   }
 
+  componentWillUnmount() {
+    if (this.request !== null) {
+      this.request.abort()
+    }
+  }
+
   _fetchItem() {
-    request
+    this.request = request
     .get(HackerNews.baseURL + "item/" + this.props.identifier + ".json")
+    // .accept('json')
     .end((err, res) => {
-      this.setState({
-        item: res.json()
-      })
-    })
+      this.setState({item: res.body})
+      this.request = null
+    });
   }
 }
 
